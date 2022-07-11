db06bdb3dd6e3ac2b4ad8e9e360a29ec5604cdbd
unknown <liangshuai_tec@126.com>
2022-07-11 13:50:03 +0800

    修改HomeView.vue

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <template>
   <div style="height:100%">
     <el-container>
+      
       <el-container>
+        <el-main>
 
         <!-- <router-view v-if="$store.state.common.refreshRoute"></router-view>         -->
      
-            <router-view ></router-view>        
+            <router-view ></router-view>  
+        </el-main>      
 
       </el-container>
 
@@ -16,12 +19,16 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-
-import {Container} from 'element-ui'
+/* eslint-disable */
+import {Container, Header, Footer, Aside,Main} from 'element-ui'
 export default {
   name: 'HomeView',
   components: {
-    'el-container':Container
+    'el-container':Container,
+    'el-header':Header,
+    'el-footer':Footer,
+    'el-aside':Aside,
+    'el-main':Main,
   }
 }
 </script>
